ce8897e3b0752519e0efed941eef9fbfbb5f675b
*y7x1IW8nNY <dunglh@guide.inc>
2021-07-27 01:22:58 +0700

    Clean code

--- blog_app_front/src/App.js
+++ blog_app_front/src/App.js
@@ -8,23 +8,19 @@ import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-  const user = false;
-  return (
-    <Router>
-      <TopBar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/register">{user ? <Home /> : <Register />}</Route>
-        <Route path="/login">{user ? <Home /> : <Login />}</Route>
-        <Route path="/write">{user ? <Write /> : <Register />}</Route>
-        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
-        <Route path="/post/:postId">
-          <Single />
-        </Route>
-      </Switch>
-    </Router>
+    const user = false;
+    return (
+        <Router>
+            <TopBar />
+            <Switch>
+                <Route exact path="/"><Home /></Route>
+                <Route path="/register">{user ? <Home /> : <Register />}</Route>
+                <Route path="/login">{user ? <Home /> : <Login />}</Route>
+                <Route path="/write">{user ? <Write /> : <Register />}</Route>
+                <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+                <Route path="/post/:postId"><Single /></Route>
+            </Switch>
+        </Router>
   )
 }
 
